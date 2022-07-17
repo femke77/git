6598c3c5c3aeefd8804edcad135d2dae518fadd2
femke77 <megan.meyers.388@gmail.com>
2022-07-16 18:39:59 -0700

    revert and work some more

--- practice.js
+++ practice.js
@@ -7,38 +7,4 @@ $(function () {
     });
   });
 
-
-  // Autocomplete widget
-$(function () {
-    var skillNames = [
-      'Bootstrap',
-      'C',
-      'C++',
-      'CSS',
-      'Express.js',
-      'Git',
-      'HTML',
-      'Java',
-      'JavaScript',
-      'jQuery',
-      'JSON',
-      'MySQL',
-      'Node.js',
-      'NoSQL',
-      'PHP',
-      'Python',
-      'React',
-      'Ruby',
-    ];
-    $('#skill-name').autocomplete({
-      source: skillNames,
-    });
-  });
-
-  // Sortable interaction
-$(function () {
-    $('#skills-list').sortable({
-      placeholder: 'ui-state-highlight',
-    });
-    $('#skills-list').disableSelection();
-  });
+  function isThisWorking() {}
