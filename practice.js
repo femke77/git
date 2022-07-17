d2ab514e797bd2a8ad16da15bc193825c62d10d2
femke77 <megan.meyers.388@gmail.com>
2022-07-16 18:25:40 -0700

    added skills name

--- practice.js
+++ practice.js
@@ -6,3 +6,30 @@ $(function () {
       changeYear: true,
     });
   });
+
+  // Autocomplete widget
+$(function () {
+    var skillNames = [
+      'Bootstrap',
+      'C',
+      'C++',
+      'CSS',
+      'Express.js',
+      'Git',
+      'HTML',
+      'Java',
+      'JavaScript',
+      'jQuery',
+      'JSON',
+      'MySQL',
+      'Node.js',
+      'NoSQL',
+      'PHP',
+      'Python',
+      'React',
+      'Ruby',
+    ];
+    $('#skill-name').autocomplete({
+      source: skillNames,
+    });
+  });
