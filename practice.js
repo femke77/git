cb9530837773a8b7fc1afd06a58cb8831146c0f7
femke77 <megan.meyers.388@gmail.com>
2023-09-28 13:42:09 -0700

    didn't pull from main before checkout to new branch and new code

--- practice.js
+++ practice.js
@@ -1,17 +1,19 @@
-
 // Datepicker widget
 $(function () {
-    $('#datepicker').datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
+  $("#datepicker").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+});
 
+function continuteWorking() {
+  console.log("hi");
+}
 
-
-
-  function continuteWorking() {
-    console.log("hi");
+function bigBlah() {
+  const arr = ["A"];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
   }
-
-
+}
